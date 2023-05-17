a7cb459bb050a633a62d40393c71bd6a6e31b020
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-18 11:49:02 +1200

    Replace GameManager.java

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,19 +1,50 @@
 
 public class GameManager {
-
+	
+	public void launchSetUpScreen() {
+		SetUpScreen setUpScreen = new SetUpScreen(this);
+	}
+	
+	public void closeSetUpScreen(SetUpScreen setUpScreen) {
+		setUpScreen.closeSetUpScreen();
+		launchDraftScreen();
+	}
+	
 	public void launchDraftScreen() {
 		DraftScreen draftWindow = new DraftScreen(this);
 	}
 	
 	public void closeDraftScreen(DraftScreen draftWindow) {
 		draftWindow.closeWindow();
-		//launchHomeScreen()
+		launchHomeScreen();
 	}
 	
+	public void launchHomeScreen() {
+		HomeScreen homeWindow = new HomeScreen(this);
+	}
+	
+	public void closeHomeScreen(HomeScreen homeWindow) {
+		homeWindow.closeWindow();
+	}
+
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		GameManager manager = new GameManager();
+		manager.launchSetUpScreen();
+		
+		//ScreenManager.setUpGame()
+		//ScreenManager.teamDraft()
+		//ScreenManager.giveNicknames()
+		/*while (Game.isRunning()) {
+			Game.generateOpposingTeams();
+			Game.generateMarketAthletes();
+			Game.generateMarketItems();
+			
+			//ScreenManager.homeScreen()
+			int week = Game.getWeek();
+			Game.setWeek(week++);*/
 	}
-
+		
 }
+    
