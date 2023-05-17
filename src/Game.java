ac3a03366e6cb1e24c4f21e6d70263aa0dca6c98
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-18 01:59:23 +1200

    Upload New File

---
+++ src/Game.java
@@ -0,0 +1,126 @@
+package senggui;
+import java.util.ArrayList;
+
+public class Game {
+	private static String playerName;
+    private static int week;
+    private static int finalWeek;
+    private static Team playerTeam;
+    private static int difficulty;
+    private static int money;
+    private static int playerRating;
+    private static int[] record; // wins, losses, byes without a game
+    private static ArrayList<Item> inventory;
+    private static ArrayList<Team> opposingTeams;
+    private static ArrayList<Item> marketItems;
+    private static ArrayList<Athlete> marketAthletes;
+    
+    public static String getPlayerName() {
+        return playerName;
+    }
+
+    public static void setPlayerName(String _playerName) {
+        playerName = _playerName;
+    }
+
+    public static int getWeek() {
+        return week;
+    }
+
+    public static void setWeek(int _week) {
+        week = _week;
+    }
+    
+    public static int getFinalWeek() {
+        return finalWeek;
+    }
+
+    public static void setFinalWeek(int _finalWeek) {
+        finalWeek = _finalWeek;
+    }
+
+    public static Team getPlayerTeam() {
+        return playerTeam;
+    }
+
+    public static void setPlayerTeam(Team _playerTeam) {
+        playerTeam = _playerTeam;
+    }
+    
+    public static int getDifficulty() {
+        return difficulty;
+    }
+
+    public static void setDifficulty(int _difficulty) {
+        difficulty = _difficulty;
+    }
+
+    public static ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    public static void setInventory(ArrayList<Item> _inventory) {
+        inventory = _inventory;
+    }
+
+    public static int getMoney() {
+        return money;
+    }
+
+    public static void setMoney(int _money) {
+        money = _money;
+    }
+
+    public static int getPlayerRating() {
+        return playerRating;
+    }
+
+    public static void setPlayerRating(int _playerRating) {
+        playerRating = _playerRating;
+    }
+
+    public static int[] getRecord() {
+        return record;
+    }
+
+    public static void setRecord(int[] _record) {
+        record = _record;
+    }
+    
+    public static boolean isRunning() {
+    	if ((week > finalWeek)) {
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
+    
+    public static void generateOpposingTeams() {
+        for (int i = 0; i < 3; i++) {
+        	Team team = Team.generateTeam(week*(difficulty+1));
+            opposingTeams.add(team);
+        }
+    }
+    
+    public static void generateMarketAthletes() {
+	    marketAthletes = new ArrayList<Athlete>();
+	    for (int i = 0; i < 5; i++) {
+	    	Athlete athlete = Athlete.generateAthlete(week);
+	        marketAthletes.add(athlete);
+	    }
+    }
+    
+    public static void generateMarketItems() {
+    	marketItems = new ArrayList<Item>();
+	    for (int i = 0; i < 5; i++) {
+	    	Item item = Item.generateItem();
+	    	marketItems.add(item);
+	    }
+    }
+    
+}
+
+
+
+
+
