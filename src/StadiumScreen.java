b10542b35cd5f288db294efb13bca331e167f1ae
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-18 12:04:25 +1200

    Upload New File

---
+++ src/StadiumScreen.java
@@ -0,0 +1,56 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class StadiumScreen {
+
+	private JFrame frame;
+	private GameManager manager;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StadiumScreen window = new StadiumScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StadiumScreen() {
+		initialize();
+	}
+
+	public StadiumScreen(GameManager incomingManager) {
+		manager = incomingManager;
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	public void finishedWindow() {
+		manager.closeStadiumScreen(this);
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
