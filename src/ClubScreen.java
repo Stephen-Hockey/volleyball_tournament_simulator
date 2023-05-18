a1e8d27be8b91c84bf075f05598461455d7f7934
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-18 12:03:35 +1200

    Upload New File

---
+++ src/ClubScreen.java
@@ -0,0 +1,56 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ClubScreen {
+
+	private JFrame frame;
+	private GameManager manager;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClubScreen window = new ClubScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ClubScreen() {
+		initialize();
+	}
+	
+	public ClubScreen(GameManager incomingManager) {
+		manager = incomingManager;
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	public void finishedWindow() {
+		manager.closeClubScreen(this);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
