28627fc2a7bcb9264d37b44f1dcb9734dd326e84
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-18 12:04:09 +1200

    Upload New File

---
+++ src/MarketScreen.java
@@ -0,0 +1,55 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MarketScreen {
+
+	private JFrame frame;
+	private GameManager manager;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MarketScreen window = new MarketScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MarketScreen() {
+		initialize();
+	}
+
+	public MarketScreen(GameManager incomingManager) {
+		manager = incomingManager;
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	public void finishedWindow() {
+		manager.closeMarketScreen(this);
+	}
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
