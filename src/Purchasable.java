050086b4d24acbb6282847351a553ac886b19a37
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-19 13:03:30 +1200

    Replace Purchasable.java

--- src/Purchasable.java
+++ src/Purchasable.java
@@ -1,19 +1,20 @@
-public abstract class Purchasable {
+package senggui;
+
+public class Purchasable {
     private String name;
     private int price;
+    private int sellPrice;
     private String description;
 
-    public static final String[] statNames = new String[] { "STAMINA", "OFFENSE", "DEFENSE" }; // im
-    // confused about
-    // calling it private
-    // static final etc
+    public static final String[] statNames = new String[] { "STAMINA", "OFFENSE", "DEFENSE" };
 
     public Purchasable() {
     }
 
-    public Purchasable(String _name, int _price, String _description) {
+    public Purchasable(String _name, int _price, int _sellPrice, String _description) {
         name = _name;
         price = _price;
+        sellPrice = _sellPrice;
         description = _description;
     }
 
@@ -32,6 +33,14 @@ public abstract class Purchasable {
     public void setPrice(int _price) {
         price = _price;
     }
+    
+    public int getSellPrice() {
+		return sellPrice;
+	}
+    
+    public void setSellPrice(int _sellPrice) {
+		sellPrice = _sellPrice;
+	}
 
     public String getDescription() {
         return description;
@@ -40,12 +49,10 @@ public abstract class Purchasable {
     public void setDescription(String _description) {
         description = _description;
     }
-
-    public String[] getStatNames() {
-        return statNames;
+    
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+    	return name;
     }
-
-    public abstract int getSellPrice();
-
-    public abstract String getMarketString();
 }
