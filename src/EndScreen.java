f122c15aa5866f994d0d978956d35755b7b01d75
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-19 13:04:29 +1200

    Upload New File

---
+++ src/EndScreen.java
@@ -0,0 +1,58 @@
+package senggui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class EndScreen {
+
+	private JFrame frame;
+	private GameManager manager;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EndScreen window = new EndScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EndScreen() {
+		initialize();
+	}
+	
+	public EndScreen(GameManager incomingManager) {
+		manager = incomingManager;
+		initialize();
+		frame.setVisible(true);
+	}
+	
+	public void closeWindow() {
+		frame.dispose();
+	}
+	
+	public void finishedWindow() {
+		manager.closeEndScreen(this);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
