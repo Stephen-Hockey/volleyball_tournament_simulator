cfa6cd0b053011d1b5098420a843bfb332ef04b7
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-19 13:09:12 +1200

    Replace Team.java

--- src/Team.java
+++ src/Team.java
@@ -1,10 +1,18 @@
+package senggui;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Team {
 
     private String teamName;
     private ArrayList<Athlete> players;
+    public static final String[] POSITION_STRINGS = new String[] { "Setter", "Outside 1", "Middle 1", "Opposite",
+            "Outside 2", "Middle 2", "Libero" };
 
     public Team() {
         teamName = "Default Team Name";
@@ -38,7 +46,6 @@ public class Team {
         players = _players;
     }
 
-    // TODO exceptions for following four methods
     public void add(Athlete athlete) {
 
         players.add(athlete);
@@ -49,8 +56,20 @@ public class Team {
         return players.get(index);
     }
 
+    public int size() {
+        return players.size();
+    }
+
+    public boolean isEmpty() {
+        return players.isEmpty();
+    }
+
     public void remove(Athlete athlete) {
         players.remove(athlete);
+    }
+
+    public void remove(int index) {
+        players.remove(index);
     }
 
     public void swap(int index1, int index2) {
@@ -65,12 +84,50 @@ public class Team {
         return details;
 
     }
+    
+    public static Team generateTeam(int quality) {
 
-    @Override
-    public String toString() {
-        return "(Team) name=\"" + teamName + "\"";
+        Random rand = new Random();
+        int n = rand.nextInt(100);
+        String animal = "";
+        try {
+            File animals = new File("src/data/animals.txt");
+            Scanner animalScanner = new Scanner(animals);
+            for (int i = 0; i <= n; i++) {
+                animal = animalScanner.nextLine();
+            }
+            animalScanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+        
+        int countryA = rand.nextInt(195);
+        String country = "";
+        try {
+            File countries = new File("src/data/countries.txt");
+            Scanner countryScanner = new Scanner(countries);
+            for (int i = 0; i <= countryA; i++) {
+                country = countryScanner.nextLine();
+            }
+            countryScanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        String teamName = "The " + country + " " + animal;
+
+        ArrayList<Athlete> players = Athlete.generateAthletes(10, quality);
+
+        return new Team(teamName, players);
     }
-
+    
+    
     public static void main(String[] args) {
-    }
+		Team t = Team.generateTeam(3);
+		
+	}
+    
+    
 }
