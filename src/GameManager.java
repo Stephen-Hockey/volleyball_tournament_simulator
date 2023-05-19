88422446c1adf9189f898fde296a583dd55073af
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-19 13:01:53 +1200

    Replace GameManager.java

--- src/GameManager.java
+++ src/GameManager.java
@@ -3,43 +3,73 @@ package senggui;
 public class GameManager {
 	
 	public void launchSetUpScreen() {
-		SetUpScreen setUpScreen = new SetUpScreen(this);
+		SetUpScreen setUpWindow = new SetUpScreen(this);
 	}
 	
-	public void closeSetUpScreen(SetUpScreen setUpScreen) {
-		setUpScreen.closeSetUpScreen();
+	public void closeSetUpScreen(SetUpScreen setUpWindow) {
+		setUpWindow.closeSetUpScreen();
 	}
 	
-	/*
-	public static void teamDraft() {
-		//calls draft window and sets the games team as the drafted team
+	public void launchDraftScreen() {
+		DraftScreen draftWindow = new DraftScreen(this);
 	}
 	
-	public static void giveNicknames() {
-		//calls nickname window and updates players with nicknames
+	public void closeDraftScreen(DraftScreen draftWindow) {
+		draftWindow.closeWindow();	
 	}
 	
-	public static void mainMenu() {
-		//shows main menu
-	}*/
+	public void launchHomeScreen() {
+		HomeScreen homeWindow = new HomeScreen(this);
+	}
 	
+	public void closeHomeScreen(HomeScreen homeWindow) {
+		homeWindow.closeWindow();
+	}
+
+	public void launchMarketScreen() {
+		MarketScreen marketWindow = new MarketScreen(this);
+	}
+	
+	public void closeMarketScreen(MarketScreen marketWindow) {
+		marketWindow.closeWindow();
+	}
+	
+	public void launchStadiumScreen() {
+		StadiumScreen stadiumWindow = new StadiumScreen(this);
+	}
+	
+	public void closeStadiumScreen(StadiumScreen stadiumWindow) {
+		stadiumWindow.closeWindow();
+	}
+	
+	public void launchEndScreen() {
+		EndScreen endWindow = new EndScreen(this);
+	}
+	
+	public void closeEndScreen(EndScreen endWindow) {
+		endWindow.closeWindow();
+	}
+	
+	public void launchClubScreen() {
+		ClubScreen clubWindow = new ClubScreen(this);
+	}
+	
+	public void closeClubScreen(ClubScreen clubWindow) {
+		clubWindow.closeWindow();
+	}
+	
+	public void launchMatchScreen() {
+		MatchScreen matchScreen = new MatchScreen(this);
+	}
+	
+	public void closeMatchScreen(MatchScreen matchScreen) {
+		matchScreen.closeWindow();
+	}
 	
 	public static void main(String[] args) {
 		
 		GameManager manager = new GameManager();
 		manager.launchSetUpScreen();
-		
-		//ScreenManager.setUpGame()
-		//ScreenManager.teamDraft()
-		//ScreenManager.giveNicknames()
-		/*while (Game.isRunning()) {
-			Game.generateOpposingTeams();
-			Game.generateMarketAthletes();
-			Game.generateMarketItems();
-			
-			//ScreenManager.homeScreen()
-			int week = Game.getWeek();
-			Game.setWeek(week++);*/
 	}
 		
 }
