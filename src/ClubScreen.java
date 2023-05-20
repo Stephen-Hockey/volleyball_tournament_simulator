20881a4212ca227ca66d353df6e7c38ea7b2a50c
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-20 18:50:18 +1200

    Replace ClubScreen.java

--- src/ClubScreen.java
+++ src/ClubScreen.java
@@ -1,6 +1,13 @@
-import java.awt.EventQueue;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class ClubScreen {
 
@@ -49,8 +56,34 @@ public class ClubScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 600, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panelTop = new JPanel();
+		panelTop.setLayout(null);
+		panelTop.setBackground(new Color(153, 193, 241));
+		panelTop.setBounds(0, 0, 600, 32);
+		frame.getContentPane().add(panelTop);
+		
+		JButton btnHelp = new JButton("?");
+		btnHelp.setBounds(538, 5, 50, 25);
+		panelTop.add(btnHelp);
+		
+		String teamName = GameEnvironment.getTeamName();
+		JLabel lblHome = new JLabel(teamName + "'s Clubhouse");
+		lblHome.setBounds(12, 10, 366, 15);
+		panelTop.add(lblHome);
+		
+		JButton btnBack = new JButton("Go Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				manager.launchHomeScreen();
+				finishedWindow();
+			}
+		});
+		btnBack.setBounds(10, 44, 117, 25);
+		frame.getContentPane().add(btnBack);
 	}
 
 }
