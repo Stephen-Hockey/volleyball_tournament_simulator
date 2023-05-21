df88a52eb36fc8b4b81f31b468b3c7e4c660a454
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-21 17:22:35 +1200

    Replace Match.java

--- src/Match.java
+++ src/Match.java
@@ -8,7 +8,8 @@ public class Match {
     private int setNumber;
     private int[][] scores;
     private int[] setScore;
-
+    private int faceoffIndex;
+    
     public Match(Team _opposingTeam) {
         matchStaminas = new ArrayList<Integer>();
         for (Athlete athlete : GameEnvironment.getPlayerTeam().getPlayers()) {
@@ -18,6 +19,7 @@ public class Match {
         setNumber = 0;
         scores = new int[5][2];
         setScore = new int[2];
+        faceoffIndex = 0;
     }
 
     public Team getOpposingTeam() {
@@ -47,7 +49,20 @@ public class Match {
     public void setSetScore(int[] _setScore) {
         setScore = _setScore;
     }
+    
+    public String getScoreString() {
+    	return String.format("%3s - %-3s", scores[setNumber][0], scores[setNumber][1]);
+    }
+    
+    public String getSetScoreString() {
+    	return String.format("%2s - %-2s", setScore[0], setScore[1]);
+    }
 
+    public int getFaceoffIndex() {
+		return faceoffIndex;
+	}
+    
+    
     public void playMatch() throws NoSubsException {
         boolean matchOver = false;
         while (!matchOver) {
@@ -87,7 +102,7 @@ public class Match {
     }
 
     private int pointWinner(int index) throws NoSubsException {
-        index = index % 7;
+    	index = index % 7;
         int winner = 0;
 
         int chanceOfWinning = 5;
@@ -136,6 +151,79 @@ public class Match {
 
         return winner;
     }
+    
+    public boolean playPoint() {
+    	
+        int winner = 0;
+
+        int chanceOfWinning = 5;
+
+        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[1] >= opposingTeam.get(faceoffIndex)
+                .getStats()[2]) {
+            chanceOfWinning += 3;
+        } else {
+            chanceOfWinning -= 1;
+        }
+
+        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[2] >= opposingTeam.get(faceoffIndex)
+                .getStats()[1]) {
+            chanceOfWinning += 1;
+        } else {
+            chanceOfWinning -= 3;
+        }
+
+        Random r = new Random();
+        if (r.nextInt(10) >= chanceOfWinning) {
+
+            matchStaminas.set(faceoffIndex, matchStaminas.get(faceoffIndex) - 1);
+            winner = 1;
+
+        }
+
+        matchStaminas.set(faceoffIndex, matchStaminas.get(faceoffIndex) - 1);
+
+        if (matchStaminas.get(faceoffIndex) <= 0) {
+            matchStaminas.set(faceoffIndex, 0);
+            GameEnvironment.getPlayerTeam().get(faceoffIndex).getStats()[0] = 0;
+            
+            return false;
+
+            /*System.out.println("Oh no! " + GameEnvironment.getPlayerTeam().get(index).getName()
+                    + " got injured facing off against " + opposingTeam.get(index).getName());
+			*/
+        }
+        
+        if (winner == 0) {
+        	scores[setNumber][0]++;
+        } else {
+        	scores[setNumber][1]++;
+        }
+        
+        faceoffIndex = (faceoffIndex + 1) % 7;
+        
+        return true;
+    }
+    
+    public boolean checkIfSetOver() {
+    	
+    	int winningScore = 25;
+    	if (setNumber == 4) {
+    		winningScore = 15;
+    	}
+    	
+    	if ((scores[setNumber][0] >= winningScore) && (scores[setNumber][0] >= scores[setNumber][1] + 2)) {
+            setScore[0]++;
+            setNumber++;
+            faceoffIndex = 0;
+            return true;
+        } else if ((scores[setNumber][1] >= winningScore) && (scores[setNumber][1] >= scores[setNumber][0] + 2)) {
+            setScore[1]++;
+            setNumber++;
+            faceoffIndex = 0;
+            return true;
+        }
+    	return false;
+    }
 
     public int getMatchRating() {
         int playerRating = (100 * GameEnvironment.getDifficulty() + 1) * (setScore[0] - setScore[1]);
@@ -164,6 +252,10 @@ public class Match {
     }
 
     public boolean matchWon() {
-        return (setScore[0] > setScore[1]);
+
+        return setScore[0] == 3;
     }
+
+	
+    
 }
