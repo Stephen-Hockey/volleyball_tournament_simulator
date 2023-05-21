3e6ac02cd271d2d6793a1de4df3fda9ec0351e93
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-22 02:03:28 +1200

    Upload New File

---
+++ src/Purchasable.java
@@ -0,0 +1,58 @@
+package senggui;
+
+public class Purchasable {
+    private String name;
+    private int price;
+    private int sellPrice;
+    private String description;
+
+    public static final String[] statNames = new String[] { "STAMINA", "OFFENSE", "DEFENSE" };
+
+    public Purchasable() {
+    }
+
+    public Purchasable(String _name, int _price, int _sellPrice, String _description) {
+        name = _name;
+        price = _price;
+        sellPrice = _sellPrice;
+        description = _description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int _price) {
+        price = _price;
+    }
+    
+    public int getSellPrice() {
+		return sellPrice;
+	}
+    
+    public void setSellPrice(int _sellPrice) {
+		sellPrice = _sellPrice;
+	}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String _description) {
+        description = _description;
+    }
+    
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+    	return name;
+    }
+}
