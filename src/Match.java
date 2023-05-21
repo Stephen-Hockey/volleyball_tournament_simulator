bd453601eed8650be787c7129434ab0047bad8fb
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-22 01:55:24 +1200

    Replace Match.java

--- src/Match.java
+++ src/Match.java
@@ -58,115 +58,26 @@ public class Match {
     	return String.format("%2s - %-2s", setScore[0], setScore[1]);
     }
 
+    public ArrayList<Integer> getMatchStaminas() {
+		return matchStaminas;
+	}
+    
     public int getFaceoffIndex() {
 		return faceoffIndex;
 	}
     
-    
-    public void playMatch() throws NoSubsException {
-        boolean matchOver = false;
-        while (!matchOver) {
-            playSet();
-
-            if ((setNumber > 4) || ((setScore[0] >= 3) || (setScore[1] >= 3))) {
-                matchOver = true;
-            }
-        }
-    }
-
-    private void playSet() throws NoSubsException {
-        int playerScore = 0;
-        int opponentScore = 0;
-        int winningScore = 25;
-        if (setNumber == 4) {
-            winningScore = 15;
-        }
-
-        boolean setOver = false;
-        while (!setOver) {
-
-            scores[setNumber][pointWinner(playerScore + opponentScore)]++;
-
-            playerScore = scores[setNumber][0];
-            opponentScore = scores[setNumber][1];
-
-            if ((playerScore >= winningScore) && (playerScore >= opponentScore + 2)) {
-                setScore[0]++;
-                setOver = true;
-            } else if ((opponentScore >= winningScore) && (opponentScore >= playerScore + 2)) {
-                setScore[1]++;
-                setOver = true;
-            }
-        }
-        setNumber++;
-    }
-
-    private int pointWinner(int index) throws NoSubsException {
-    	index = index % 7;
-        int winner = 0;
-
-        int chanceOfWinning = 5;
-
-        if (GameEnvironment.getPlayerTeam().get(index).getEffectiveStats()[1] >= opposingTeam.get(index)
-                .getStats()[2]) {
-            chanceOfWinning += 3;
-        } else {
-            chanceOfWinning -= 1;
-        }
-
-        if (GameEnvironment.getPlayerTeam().get(index).getEffectiveStats()[2] >= opposingTeam.get(index)
-                .getStats()[1]) {
-            chanceOfWinning += 1;
-        } else {
-            chanceOfWinning -= 3;
-        }
-
-        Random r = new Random();
-        if (r.nextInt(10) >= chanceOfWinning) {
-
-            matchStaminas.set(index, matchStaminas.get(index) - 1);
-            winner = 1;
-
-        }
-
-        matchStaminas.set(index, matchStaminas.get(index) - 1);
-
-        if (matchStaminas.get(index) <= 0) {
-            matchStaminas.set(index, 0);
-            GameEnvironment.getPlayerTeam().get(index).getStats()[0] = 0; // actually injure them lol
-
-            System.out.println("Oh no! " + GameEnvironment.getPlayerTeam().get(index).getName()
-                    + " got injured facing off against " + opposingTeam.get(index).getName());
-
-            int subIndex = GameFunction.getSubIndex(index);
-
-            Collections.swap(matchStaminas, index, subIndex);
-            GameEnvironment.getPlayerTeam().swap(index, subIndex);
-
-            System.out.println(
-                    "Subbed " + GameEnvironment.getPlayerTeam().get(subIndex).getNameWithNickname()
-                            + " in for " + GameEnvironment.getPlayerTeam().get(index).getNameWithNickname());
-
-        }
-
-        return winner;
-    }
-    
     public boolean playPoint() {
     	
         int winner = 0;
-
         int chanceOfWinning = 5;
 
-        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[1] >= opposingTeam.get(faceoffIndex)
-                .getStats()[2]) {
+        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[1] >= opposingTeam.get(faceoffIndex).getStats()[2]) {
             chanceOfWinning += 3;
         } else {
             chanceOfWinning -= 1;
         }
 
-        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[2] >= opposingTeam.get(faceoffIndex)
-                .getStats()[1]) {
+        if (GameEnvironment.getPlayerTeam().get(faceoffIndex).getEffectiveStats()[2] >= opposingTeam.get(faceoffIndex).getStats()[1]) {
             chanceOfWinning += 1;
         } else {
             chanceOfWinning -= 3;
@@ -174,10 +85,8 @@ public class Match {
 
         Random r = new Random();
         if (r.nextInt(10) >= chanceOfWinning) {
-
             matchStaminas.set(faceoffIndex, matchStaminas.get(faceoffIndex) - 1);
             winner = 1;
-
         }
 
         matchStaminas.set(faceoffIndex, matchStaminas.get(faceoffIndex) - 1);
@@ -187,10 +96,6 @@ public class Match {
             GameEnvironment.getPlayerTeam().get(faceoffIndex).getStats()[0] = 0;
             
             return false;
-
-            /*System.out.println("Oh no! " + GameEnvironment.getPlayerTeam().get(index).getName()
-                    + " got injured facing off against " + opposingTeam.get(index).getName());
-			*/
         }
         
         if (winner == 0) {
@@ -252,10 +157,7 @@ public class Match {
     }
 
     public boolean matchWon() {
-
-        return setScore[0] == 3;
+        return (setScore[0] == 3);
     }
 
-	
-    
 }
