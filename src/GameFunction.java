6771cb06d89152db038eaeec37925cbc55592dfa
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-21 17:21:19 +1200

    Replace GameFunction.java

--- src/GameFunction.java
+++ src/GameFunction.java
@@ -569,4 +569,12 @@ public class GameFunction {
         System.out.println("Player Rating: " + GameEnvironment.getPlayerRating());
         System.out.println("Thanks for playing!!!\n");
     }
+    public static void main(String[] args) {
+    	int i = 2;
+    	int itemp = i;
+		do {
+			System.out.println(""+i);
+			i = (i + 1) % 7;
+		} while (i !=  itemp);
+	}
 }
