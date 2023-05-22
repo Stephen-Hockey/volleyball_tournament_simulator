39691424e3fa7398060fbc1b08b2c272192ebaf6
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-22 13:21:52 +1200

    Completed the java doc

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -1,34 +1,100 @@
 
 import java.util.ArrayList;
 
+/**
+ * This class implements the environment for the game
+ * all current game variables and methods are stored 
+ *
+ * @author Lachlan Stewart and Stephen Hockey
+ * @version 1.1, May 2023.
+ */
+
 public class GameEnvironment {
+	
+	/**
+	 * The name of the Player/User
+	 */
     private static String playerName;
+    /**
+	 * The current week of the game
+	 */
     private static int week;
+    /**
+	 * The number of weeks given by the user for game duration
+	 */
     private static int finalWeek;
+    /**
+	 * The Player/User's Team
+	 */
     private static Team playerTeam;
+    /**
+	 * The game difficulty
+	 */
     private static int difficulty;
+    /**
+	 * The Player/User's money
+	 */
     private static int money;
+    /**
+	 * The Player/User's current games rating
+	 */
     private static int playerRating;
-    private static int[] record; // wins, losses, byes without a game
+    /**
+	 * A record of the current game's wins, losses and byes without a game {wins, losses, byes}
+	 */
+    private static int[] record;
+    /**
+	 * The Player/User's items
+	 */
     private static ArrayList<Item> inventory;
+    /**
+	 * The Athletes currently available to purchase in the marketplace
+	 */
     private static ArrayList<Athlete> currentWeekMarketAthletes;
+    /**
+	 * The Items currently available to purchase in the marketplace
+	 */
     private static ArrayList<Item> currentWeekMarketItems;
+    /**
+   	 * The Opponents currently available to play in the Stadium
+   	 */
     private static ArrayList<Team> currentWeekOpposingTeams;
+    /**
+   	 * A record of past matches against opponent teams
+   	 */
     private static ArrayList<Match> matches;
-    private static boolean gameRunning;
+    /**
+   	 * A record of if the Player/User has played their weekly alloted game
+   	 */
     private static boolean weeklyGamePlayed;
+    /**
+   	 * Represents if the Player/User successfully completed the game
+   	 */
     private static boolean gameSuccess;
-    
+    /**
+   	 * Sets the maximum number of players and items allowed in the Player/User's team
+   	 */
     public static final int MAX_PLAYERS = 12, MAX_ITEMS= 12;
 
+    
+    
+    /**
+   	 * Returns the Player/User's name
+   	 * @return the Player/User's name
+   	 */
     public static String getPlayerName() {
         return playerName;
     }
 
+    
     public static void setPlayerName(String _playerName) {
         playerName = _playerName;
     }
 
+    /**
+   	 * Returns the current in game week as an integer
+   	 * @return the current game week as an integer
+   	 */
     public static int getWeek() {
         return week;
     }
@@ -37,6 +103,10 @@ public class GameEnvironment {
         week = _week;
     }
 
+    /**
+   	 * Returns the chosen game length as an integer
+   	 * @return game length as an integer
+   	 */
     public static int getFinalWeek() {
         return finalWeek;
     }
@@ -45,6 +115,10 @@ public class GameEnvironment {
         finalWeek = _finalWeek;
     }
 
+    /**
+   	 * Returns the Player/User's Team as a Team object
+   	 * @return the Player/User's Team as a Team object
+   	 */
     public static Team getPlayerTeam() {
         return playerTeam;
     }
@@ -52,15 +126,24 @@ public class GameEnvironment {
     public static void setPlayerTeam(Team _playerTeam) {
         playerTeam = _playerTeam;
     }
-
+    
+    /**
+   	 * Returns the chosen difficulty as an integer
+   	 * @return the chosen difficulty as an integer
+   	 */
     public static int getDifficulty() {
         return difficulty;
     }
 
+    
     public static void setDifficulty(int _difficulty) {
         difficulty = _difficulty;
     }
 
+    /**
+   	 * Returns the Player/User's items as an ArrayList of Item objects
+   	 * @return the Player/User's items as an ArrayList of Item objects
+   	 */
     public static ArrayList<Item> getInventory() {
         return inventory;
     }
@@ -69,6 +152,10 @@ public class GameEnvironment {
         inventory = _inventory;
     }
 
+    /**
+   	 * Returns the Player/User's money as an integer
+   	 * @return the Player/User's money as an integer
+   	 */
     public static int getMoney() {
         return money;
     }
@@ -77,6 +164,10 @@ public class GameEnvironment {
         money = _money;
     }
 
+    /**
+   	 * Returns the Player/User's rating as an integer
+   	 * @return the Player/User's rating as an integer
+   	 */
     public static int getPlayerRating() {
         return playerRating;
     }
@@ -84,35 +175,40 @@ public class GameEnvironment {
     public static void setPlayerRating(int _playerRating) {
         playerRating = _playerRating;
     }
-
+    
+    /**
+   	 * Returns the current game's wins, losses and byes as an integer list of the form {wins, losses, byes}
+   	 * @return the current game's wins, losses and byes as an integer list of the form {wins, losses, byes}
+   	 */
     public static int[] getRecord() {
         return record;
     }
-
     public static void setRecord(int[] _record) {
         record = _record;
     }
     
+    /**
+   	 * Returns the Player/User's past match record as an ArrayList of Match objects
+   	 * @return the Player/User's past match record as an ArrayList of Match objects
+   	 */
     public static ArrayList<Match> getMatches() {
 		return matches;
 	}
+    
     
     public static void setMatches(ArrayList<Match> _matches) {
 		GameEnvironment.matches = _matches;
 	}
     
+    
     public static void addMatch(Match match) {
     	GameEnvironment.matches.add(match);
     }
-
-    public static boolean getGameRunning() {
-        return gameRunning;
-    }
-
-    public static void setGameRunning(boolean _gameRunning) {
-        gameRunning = _gameRunning;
-    }
-    
+ 
+    /**
+   	 * Returns the value of the weekly game played as a boolean
+   	 * @return the value of the weekly game played as a boolean
+   	 */
     public static boolean getWeeklyGamePlayed() {
     	return weeklyGamePlayed;
     }
@@ -121,6 +217,10 @@ public class GameEnvironment {
     	weeklyGamePlayed = _weeklyGamePlayed;
     }
     
+    /**
+   	 * Returns the Player/User successfully completed the game as a boolean
+   	 * @return the Player/User successfully completed the game as a boolean
+   	 */
     public static boolean getGameSuccess() {
     	return gameSuccess;
     }
@@ -129,19 +229,34 @@ public class GameEnvironment {
     	gameSuccess = _gameSuccess;
     }
     
-    
+    /**
+   	 * Returns the current athletes available for purchase from the marketplace an ArrayList of Athlete objects
+   	 * @return the current athletes available for purchase from the marketplace an ArrayList of Athlete objects
+   	 */
     public static ArrayList<Athlete> getCurrentWeekMarketAthletes() {
 		return currentWeekMarketAthletes;
 	}
     
+    /**
+   	 * Returns the current items available for purchase from the marketplace an ArrayList of Item objects
+   	 * @return the current items available for purchase from the marketplace an ArrayList of Item objects
+   	 */
     public static ArrayList<Item> getCurrentWeekMarketItems() {
 		return currentWeekMarketItems;
 	}
     
+    /**
+   	 * Returns the Opponents currently available to play in the Stadium as an ArrayList of Team objects
+   	 * @return the Opponents currently available to play in the Stadium as an ArrayList of Team objects
+   	 */
     public static ArrayList<Team> getCurrentWeekOpposingTeams() {
 		return currentWeekOpposingTeams;
     }
     
+    /**
+   	 * If the Player/User can make a valid team with their current athletes as a boolean
+   	 * @return If the Player/User can make a valid team with their current athletes as a boolean
+   	 */
     public static boolean hasFullTeam() {
     	int healthyPlayers = 0;
     	for (Athlete athlete : playerTeam.getPlayers()) {
@@ -152,6 +267,10 @@ public class GameEnvironment {
     	return healthyPlayers >= 7;
     }
     
+    /**
+   	 * If the Player/User's current line up is valid as a boolean
+   	 * @return If the Player/User's current line up is valid as a boolean
+   	 */
     public static boolean hasHealthyStarters() {
     	for (int i = 0; i < 7; i++) {
     		if (playerTeam.get(i).getStats()[0] == 0) {
@@ -161,6 +280,9 @@ public class GameEnvironment {
     	return true;
     }
     
+    /**
+   	 * Sets up all values for the next week, such as, renewing the market, generating new opponents and increments the week counter
+   	 */
     public static void setUpWeek() {
     	currentWeekMarketAthletes = new ArrayList<Athlete>();
     	currentWeekMarketItems = new ArrayList<Item>();
