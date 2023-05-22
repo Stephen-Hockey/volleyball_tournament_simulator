70c66f94f0f06cb7b03d3cdfe6138b0a379e19bc
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-23 00:22:55 +1200

    Replace SetUpScreenTest.java

--- test/SetUpScreenTest.java
+++ test/SetUpScreenTest.java
@@ -4,11 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import main.SetUpScreen;
+
 public class SetUpScreenTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String[] inputs = {"  !@## abc", "aa", "abcdefghijklmnopqrstuvwxyz"};
+		SetUpScreen window = new SetUpScreen();
+		for (String input: inputs) {
+			assertEquals(false, window.validString(input, 3, 10));
+		}
 	}
 
 }
