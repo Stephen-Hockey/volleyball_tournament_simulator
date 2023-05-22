c607125ff265880592b67b31e91c17f5013d5599
Lachlan Stewart <lst117@uclive.ac.nz>
2023-05-22 23:53:50 +1200

    Replace AthleteTest.java

--- test/AthleteTest.java
+++ test/AthleteTest.java
@@ -2,15 +2,53 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import main.Athlete;
+import main.Item;
 
 public class AthleteTest {
 
-	/*@Test
-	public void getSellPrice() {
-	    Athlete testAthlete = new Athlete();
-	    testRocketShip.fuelUp(30);
-	    assertEquals(80, testRocketShip.getFuelLevel());
-	}*/
+	@Test
+	public void getSellPriceTest() {
+		Athlete testAthlete = new Athlete();
+	    testAthlete.setPrice(100);
+	    int sellPrice = testAthlete.getSellPrice();
+	    assertEquals(80, sellPrice);
+	}
+	
+	@Test
+	public void addItemTest() {
+		Athlete testAthlete = new Athlete();
+		int[] initialAthleteStats = {1, 1, 0};
+		testAthlete.setStats(initialAthleteStats);
+		Item testItem = new Item();
+		int[] initialItemStats = {0, 0, 1};
+		testItem.setStats(initialItemStats);
+		testAthlete.addItem(testItem);
+		assertArrayEquals(new int[]{1, 1, 1}, testAthlete.getStats());
+	}
+	
+	@Test
+	public void getAvgStatTest() {
+		Athlete testAthlete = new Athlete();
+		int[] initialAthleteStats = {4, 5, 6};
+		testAthlete.setStats(initialAthleteStats);
+		assertEquals(5, testAthlete.getAvgStat());
+	}
+	
+	@Test
+	public void generateAthleteTest() {
+		Athlete testAthlete = Athlete.generateAthlete(5);
+		assertEquals(testAthlete.getClass(), Athlete.class);
+	}
+	
+	@Test
+	public void generateAthletesTest() {
+		ArrayList<Athlete> testAthletes = Athlete.generateAthletes(22, 0);
+		assertEquals(22, testAthletes.size());
+	}
 
 }
