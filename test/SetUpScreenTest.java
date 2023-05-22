a65918e76c6c5cfda835e75c7aaa8cd587358ff3
Stephen Hockey <sho151@uclive.ac.nz>
2023-05-23 05:14:29 +1200

    Replace SetUpScreenTest.java

--- test/SetUpScreenTest.java
+++ test/SetUpScreenTest.java
@@ -6,15 +6,30 @@ import org.junit.Test;
 
 import main.SetUpScreen;
 
-public class SetUpScreenTest {
+public class SetupScreenTest {
 
 	@Test
-	public void test() {
-		String[] inputs = {"  !@## abc", "aa", "abcdefghijklmnopqrstuvwxyz"};
-		SetUpScreen window = new SetUpScreen();
-		for (String input: inputs) {
-			assertEquals(false, window.validString(input, 3, 10));
-		}
+	public void validStringTest() {
+		SetUpScreen window = new SetUpScreen(null);
+		assertTrue(window.validString("John Smith", 3, 10));
+	}
+
+	@Test
+	public void validStringSpecialCharactersTest() {
+		SetUpScreen window = new SetUpScreen(null);
+		assertFalse(window.validString("J*hn/$mith", 3, 10));
+	}
+	
+	@Test
+	public void validStringTooLongTest() {
+		SetUpScreen window = new SetUpScreen(null);
+		assertFalse(window.validString("Johnathan Smithington", 3, 10));
+	}
+	
+	@Test
+	public void validStringTooShortTest() {
+		SetUpScreen window = new SetUpScreen(null);
+		assertFalse(window.validString("JS", 3, 10));
 	}
 
 }
